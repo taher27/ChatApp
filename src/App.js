a3b39e568de5268a1594f7f2547cd2496c3a0c8c
taher27 <mandapwalataher@gmail.com>
2019-06-21 12:54:29 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Chatkit from '@pusher/chatkit';
+import MessageList from './component/MessageList';
+import SendMessageForm from './component/SendMessageForm';
+import RoomList from './component/RoomList';
+import NewRoomForm from './component/NewRoomForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { tokenUrl, instanceLocator } from './config';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      roomId:  null,
+      messages: [],
+      joinableRooms: [],
+      joinedRooms: []
+    }
+    this.sendMessage = this.sendMessage.bind(this);
+    this.subscribeToRoom = this.subscribeToRoom.bind(this);
+    this.getRooms = this.getRooms.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
+  
+  componentDidMount(){
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'Aryan',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+    chatManager.connect()
+    .then(currentUser => {
+      this.currentUser = currentUser
+      this.getRooms()
+    })
+    .catch(err => console.log('error on Connecting: ', err))
+  }
+
+  getRooms() {
+    this.currentUser.getJoinableRooms()
+      .then(joinableRooms => {
+        this.setState({
+          joinableRooms,
+          joinedRooms: this.currentUser.rooms
+        })
+      })
+      .catch(err => console.log('error on joinableRooms: ', err))
+  }
+
+  subscribeToRoom(roomId) {
+    this.setState({ messages: [] })
+    this.currentUser.subsribeToRoom({
+      roomId: roomId,
+      hooks: {
+        onNewMessage: message => {
+           this.setState({
+            messages: [...this.state.messages, message]
+          })
+        }
+      }
+    }) 
+    .then(room => {
+      this.setState({
+        roomId: room.id
+      })
+      this.getRooms()
+    })
+    .catch(err => console.log('error on subscribing to room: ', err))
+  }
+
+  sendMessage(text) {
+    this.currentUser.sendMessage({
+      text,
+      roomId: this.state.roomId
+      })
+  }
+
+  createRoom(name) {
+    this.currentUser.createRoom({
+      name
+    })
+    .then(room => this.subscribeToRoom(room.id))
+    .catch(err => console.log('error with createRoom: ', err))
+  }
+
+  render() {
+    console.log(this.state.messages);
+    return(
+      <div className="app">
+        <RoomList 
+            roomId = {this.state.roomId}
+            subscribeToRoom={this.subscribeToRoom} 
+            rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} />
+
+        <MessageList 
+            roomId={this.state.roomId}
+            messages={this.state.messages}/>
+        
+        <SendMessageForm 
+            disabled={!this.state.roomId}
+            sendMessage={this.sendMessage}/>
+        
+        <NewRoomForm createRoom={this.createRoom} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+// const tokenUrl = 
+// "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4c76c8a9-4f3e-4e96-a92b-3a9dbbc75169/token";
+// const instanceLocator = "v1:us1:4c76c8a9-4f3e-4e96-a92b-3a9dbbc75169";
+
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/31e2c567-cec2-4bca-839e-ec071e9f9eb9/token"
+const instanceLocator = "v1:us1:31e2c567-cec2-4bca-839e-ec071e9f9eb9"
+
+export { tokenUrl, instanceLocator }
